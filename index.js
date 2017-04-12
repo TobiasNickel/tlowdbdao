4315c1524c55f800554b68352ea045ada32bce11
Tobias Nickel <ross40k@googlemail.com>
2017-04-12 23:28:33 +0800

    all test passed

---
+++ index.js
@@ -0,0 +1,198 @@
+const low = require('lowdb');
+const newPromise = require('./lib/newPromise');
+const first = require('./lib/promiseFirst');
+const prepareFetchMethod = require('./lib/prepareFetchMethod');
+const valueIn = require('./lib/valueIn');
+//var prepareConditionalMethod = require('./lib/prepareConditionalMethod');
+
+module.exports = function(params) {
+    var db = low(params.path);
+
+    const database = {
+        db: db,
+        daos: params.daos || {},
+        defaultPrimaryName: 'id',
+
+        newPromise: params.newPromise || newPromise,
+
+        getBy: function(tableName, fieldName, value /*, order */ ) {
+            if (!Array.isArray(value)) value = [value];
+            const filter = {};
+            filter[fieldName] = value;
+            return Promise.resolve(database.db.get(tableName).filter(function(item) {
+                return value.indexOf(item[fieldName]) !== -1;
+            }).value());
+        },
+
+        getOneBy: function(tableName, fieldName, value /*, order */ ) {
+            return first(database.getBy(tableName, fieldName, value));
+        },
+
+        where: function(tableName, obj, page, pageSize) {
+            if (!pageSize) pageSize = 10;
+            var collection = database.db.get(tableName);
+            var result = collection.filter(obj);
+            if (page) {
+                collection.slice(page * pageSize, page * pageSize + pageSize)
+            }
+            return Promise.resolve(collection.value());
+        },
+
+        remove: function() {},
+
+        createTable: function() {},
+
+        /**
+         * @param {object} dao
+         */
+        prepareDao: function(dao) {
+            var tableName = dao.tableName;
+            database.daos[dao.tableName] = dao;
+
+            var IDKeys = [];
+
+            if (typeof dao.map !== 'function') {
+                dao.map = function(item) { return item; };
+            }
+            if (typeof dao.inputMap !== 'function') {
+                dao.inputMap = function(item) { return item; };
+            }
+            dao.inputMapAll = function(items) {
+                if (Array.isArray(items)) {
+                    return items.map(function(item) {
+                        return dao.inputMap(item)
+                    });
+                } else {
+                    return dao.inputMap(items);
+                }
+            }
+
+            dao.promiseMap = function(p) {
+                return p.then(function(items) {
+                    if (typeof items !== 'object') return items;
+                    if (Array.isArray(items)) {
+                        var res = items.map(dao.map);
+                        res.resultCount = items.resultCount;
+                        res.pageCount = items.pageCount;
+                        return res;
+                    } else {
+                        //one item
+                        return dao.map(items)
+                    }
+                });
+            };
+
+            dao.insert = function(object) {
+                console.assert(typeof object === 'object', 'object need to be an object')
+                console.log('insert', object);
+                database.db.get(tableName).push(object).write();
+                return Promise.resolve();
+            };
+
+            dao.save = function(objs, connection) {
+                if (!Array.isArray(objs)) objs = [objs];
+                var tasks = objs.map(obj => dao.saveOne(obj));
+                return Promise.all(tasks);
+            };
+            dao.saveOne = function(obj) {
+                var search = {};
+                IDKeys.forEach(function(key) {
+                    search[key] = obj[key];
+                });
+                database.db.get('posts')
+                    .find(search)
+                    .assign(obj)
+                    .write();
+                return Promise.resolve();
+            }
+
+
+            dao.set = function(update, objs) { return Promise.resolve(); }
+
+            dao.getAll = function() {
+                var all = database.db.get(tableName).value() || [];
+                console.log('getAll', all)
+                return Promise.resolve(all);
+            }
+            dao.findWhere = function(obj, page, pageSize) {
+                var promise = database.where(tableName, obj, page, pageSize);
+                return dao.promiseMap(promise);
+            };
+            dao.findOneWhere = function(obj, page, pageSize) {
+                var promise = database.where(tableName, obj);
+                return dao.promiseMap(promise);
+            };
+            dao.where = function(where, params, page, pageSize, connection) {
+                var promise = database.db.where(tableName, where, params, page, pageSize, connection);
+                return dao.promiseMap(promise);
+            };
+            dao.oneWhere = function(where, params, page, pageSize, connection) {
+                var promise = database.db.where(tableName, where, params, page, pageSize, connection);
+                return dao.promiseMap(promise);
+            };
+            dao.remove = function(obj, connection) {
+                return database.remove(tableName, IDKeys, obj, connection);
+                //this.db.resolve();
+            };
+            dao.dropTable = function() { return Promise.resolve(); };
+            dao.createTable = function() {
+                //todo, if not exists
+                console.log('create table', tableName)
+                database.db.set(tableName, []).write();
+                return Promise.resolve();
+            };
+            var fieldNames = Object.keys(dao.fields);
+            fieldNames.forEach(function(name) {
+
+                var filterCreator = valueIn(name);
+
+                var definition = dao.fields[name];
+                var addName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+                dao['getBy' + addName] = function(value /*, page, pageSize, connection*/ ) {
+                    var valueFilter = filterCreator(value);
+                    var result = database.db.get(tableName).filter(valueFilter).value();
+                    var promise = Promise.resolve(result);
+                    //var promise = this.db.getBy(tableName, name, value, page, pageSize, connection);
+                    return promise; //dao.promiseMap(promise);
+                };
+
+                dao['getOneBy' + addName] = function(value) {
+                    var valueFilter = filterCreator(value);
+                    return first(this['getBy' + addName](value));
+                };
+
+                dao['removeBy' + addName] = function(value, connection) {
+                    var valueFilter = filterCreator(value);
+                    database.db.remove(not(valueFilter)).write();
+                    return Promise.resolve();
+                    //return this.db.remove(tableName, name, value, connection);
+                };
+                if (definition.mapTo) {
+                    prepareFetchMethod(database, dao, tableName, name, definition);
+                }
+                if (definition.primary) { IDKeys.push(name); }
+            });
+            if (dao.has) {
+                for (var name in dao.has) {
+                    prepareFetchMethod(database, dao, tableName, name, { mapTo: dao.has[name] });
+                }
+            }
+            // if (dao.conditionals) {
+            //     for (var name in dao.conditionals) {
+            //         prepareConditionalMethod(database, dao, tableName, name, dao.conditionals[name]);
+            //     }
+            // }
+
+            return dao;
+        }
+    };
+
+    return database;
+}
+
+function not(f) {
+    return function(v) {
+        return !f(v)
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,189 @@
+var tdao = require('../index');
+
+var db = tdao({ path: __dirname + '/testdb.json' });
+
+var userDao = db.prepareDao({
+    tableName: 'users',
+    fields: {
+        id: { type: 'INT AUTO_INCREMENT', primary: true },
+        name: { type: 'varchar(30)' },
+        mail: { type: 'varchar(255)' },
+        password: { type: 'char(34)' },
+        registered: { type: 'INT' }
+    },
+    has: {
+        images: { tableName: 'images', foreignKey: 'owner', localField: 'id', multiple: true },
+    },
+    // conditionals: {
+    //     new: { condition: 'TO_DAYS(registered) > (TO_DAYS(NOW())-10)', multiple: true } // to provide a getNew method that return the users registered in the last 10 days
+    // },
+    // queries: {
+    //     withoutPicture: 'SELECT * FROM users WHERE id NOT IN (SELECT distinct owner FROM images)',
+    //     byPassword: 'SELECT * FROM users WHERE password = ?'
+    // }
+});
+var imageDao = db.prepareDao({
+    tableName: 'images',
+    fields: {
+        id: { type: 'INT AUTO_INCREMENT', primary: true },
+        name: { type: 'varchar(30)' },
+        path: { type: 'varchar(255)' },
+        owner: { type: 'int', mapTo: { tableName: 'users', foreignKey: 'id' } },
+        uploadTime: { type: 'datetime' }
+    },
+    has: {
+        likes: { tableName: 'likes', foreignKey: 'image', localField: 'id', multiple: false },
+    },
+    // conditionals: {
+    //     newest: { condition: 'not exists (SELECT * from images as i where i.uploadTime>uploadTime )', multiple: false },
+    //     new: { condition: 'TO_DAYS(uploadTime) > (TO_DAYS(NOW())-10)', multiple: true } // to provide a getNew method that return the users registered in the last 10 days
+    // }
+});
+var likeDao = db.prepareDao({
+    tableName: 'likes',
+    fields: {
+        user: { type: 'INT', primary: true, mapTo: { tableName: 'users', foreignKey: 'id' } },
+        image: { type: 'INT', primary: true, mapTo: { tableName: 'images', foreignKey: 'id' } },
+        time: { type: 'datetime' }
+    },
+    // conditionals: {
+    //     new: { condition: 'TO_DAYS(time) > (TO_DAYS(NOW())-10)' } // to provide a getNew method that return the users registered in the last 10 days
+    // }
+});
+
+db.logQueries = false;
+
+console.log('prepare dao: DONE');
+var users = null;
+var objs = null;
+
+likeDao.dropTable()
+    .then(function(newImages) {
+        return userDao.dropTable();
+    })
+    .then(function(images) {
+        return imageDao.dropTable();
+    })
+    .then(function(images) {
+        return userDao.createTable();
+    })
+    .then(function() {
+        console.log('createTable: DONE')
+        return imageDao.createTable();
+    })
+    .then(function() {
+        return likeDao.createTable();
+    })
+    .then(function() {
+        console.log('createTable: DONE')
+        return userDao.insert({ name: 'tobias', mail: 'business@tnickel.de', password: 'kvckxchlksjfhliu', registered: 1 });
+    })
+    .then(function() {
+        console.log('insert object: DONE')
+        return likeDao.insert({ user: 0, image: 0, time: new Date() });
+    })
+    .then(function() {
+        console.log('insert object: DONE')
+        return userDao.getAll();
+    })
+    .then(function(_users) {
+        users = _users;
+        var user1 = users[0];
+        console.log(db.db.getState())
+        return imageDao.insert({ name: 'superman.jpg', path: 'images/1/superman.jpg', owner: user1.id, uploadTime: new Date() })
+    })
+    .then(function() {
+        users[0].name = 'Tobias';
+        return userDao.save(users[0])
+    })
+    .then(function() {
+        return likeDao.fetchImage(users[0])
+    })
+    .then(function() {
+        return userDao.saveOne(users[0])
+    })
+    .then(function() {
+        return userDao.getOneById(0)
+    })
+    .then(function() {
+        return userDao.findWhere({ id: 0 })
+    })
+    .then(function() {
+        return userDao.getById(0)
+    })
+    .then(function() {
+        console.log('get all object: DONE');
+        return userDao.fetchImages(users);
+    })
+    .then(function(_users) {
+        return userDao.fetchImages('0', 'some param', 'second useless param');
+    })
+    .then(function() {
+        return userDao.fetchImages([]);
+    })
+    // .then(function() {
+    //     console.log('get all object: DONE');
+    //     return imageDao.getNewest();
+    // })
+    // .then(function() {
+    //     console.log('get all object: DONE');
+    //     return imageDao.getNew();
+    // })
+    .then(function(newImages) {
+        //console.log('load conditional Images: DONE', newImages.length == 1);
+        //console.log(JSON.stringify(users,null,'  '))
+        return imageDao.remove(newImages);
+    })
+    .then(function() {
+        return imageDao.removeById(4);
+    })
+    .then(function(connection) {
+        return likeDao.remove({ user: 0, image: 0 });
+    })
+    // .then(function(connection) {
+    //     return db.save('usas', 'id', { name: 'unchanges' }).catch(function(err) {
+    //         console.log('catched Error: DONE')
+    //     });
+    // })
+    // .then(function(connection) {
+    //     //selectPaged: function (sql, values, page, pagesize, connection)
+    //     return db.selectPaged('SELECT * FROM usas', 0, 1).catch(function(err) {
+    //         console.log('catched Error: DONE')
+    //     });
+    // })
+    .then(function(newImages) {
+        return imageDao.findOneWhere({ name: 'superman.jpg' });
+    })
+    // .then(function(newImages) {
+    //     return userDao.withoutPicture();
+    // })
+    // .then(function(newImages) {
+    //     return userDao.withoutPicture(0, 2);
+    // })
+    .then(function(newImages) {
+        return userDao.set({ registered: 1 }, [1]);
+    })
+    .then(function(newImages) {
+        return likeDao.set({ time: 1 }, { user: 1, image: 1 });
+    })
+    .then(function(newImages) {
+        return userDao.dropTable();
+    })
+    .then(function(images) {
+        return imageDao.dropTable();
+    })
+    .then(function(images) {
+        return likeDao.dropTable()
+            .then(function() {
+                //require('./arrayTest')();
+                //require('./countMysqlParameterTest')();
+                //require('./isConnectionTest')();
+                // return require('./prepareFetchMethodTest')()
+                //     .then(function() {
+                //         process.exit()
+                //     });
+            });
+    })
+    .catch(function(err) {
+        console.log('failed:', err, err.stack);
+    });
